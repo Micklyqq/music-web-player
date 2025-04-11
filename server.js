ab3087a7b2c6ac923c4d377b569c6dbf3a599375
Mickly <maykiqq@gmail.com>
2025-04-11 14:55:05 +0300

    create uploadDir if doesn't exist

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(cors());
 
 const uploadDir = path.join(__dirname, "uploads");
 
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadDir);
